9ae79fc417dc46512ade742d077ada135d2e1409
Blien Habtu <bhabtu622@gmail.com>
2025-03-05 17:31:02 -0500

    feat: Add Note model and serializer for note creation

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# ORM allows defining models in Python, which it then converts to database schema.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()  # No max length
+    created_at = models.DateTimeField(auto_now_add=True)  # Auto-populated on creation
+
+    # One-to-many: a user can have many notes. Deleting user deletes their notes.
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+
+    def __str__(self):
+        return self.title
